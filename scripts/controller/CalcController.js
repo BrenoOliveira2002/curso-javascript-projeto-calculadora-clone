4f5801a4aefe226d2381b428d214c29605d0072d
BrenoOliveira2022 <breno_2002_@outlook.com>
2022-05-09 10:38:34 -0300

    adding keyboard events

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -11,6 +11,7 @@ class CalcController {
         this._currentDate;
         this.initialize();
         this.initButtonsEvents();
+        this.initKeyboard();
     }
     //arrow function para ficar recarregando o horário de 1 em 1 sec
     initialize(){  
@@ -18,6 +19,74 @@ class CalcController {
         this.setDisplayTime();
         },1000);  
         this.setLastNumberToDisplay();
+
+     }
+
+     initKeyboard(){
+
+        document.addEventListener('keyup', e => {
+            console.log(e.key);
+
+            switch (e.key) {
+
+                case 'Escape':
+        
+                    this.clearAll();
+        
+                break;
+        
+                case 'Backspace':
+                    
+                    this.clearEntry();
+        
+                break;
+        
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '%':
+
+                    this.addOperation(e.key);
+        
+                break;
+        
+                case 'Enter':
+                case '=':
+                    
+                    this.calc();
+        
+                break;
+                
+                case 'porcento':
+                    
+                    this.addOperation('%');
+        
+                break;
+        
+                case '.':
+                case ',':
+                     this.addDot('.');
+        
+                break;
+        
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+        
+                    this.addOperation(parseInt(e.key));
+        
+                break;
+        
+            }
+         })
 
      }
 
